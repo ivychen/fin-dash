955d54980fe3c0fae7861ad9420cf7da98122261
ivychen <ivycny@gmail.com>
2016-09-24 10:17:55 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+	return render_template('index.html')
+
+@app.route("showSignup")
+def showSignup():
+	return render_template('signup.html')
+
+@app.route
+
+if __name__ == "__main__":
+	app.run(debug=True)

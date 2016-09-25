6b3f2fd8136dfb2432c170d3e0dffdf0f6ae57e4
ivychen <ivycny@gmail.com>
2016-09-24 22:50:49 -0400

    slider ionden

--- static/js/custom-script.js
+++ static/js/custom-script.js
@@ -11,7 +11,22 @@ PLACE HERE YOUR OWN JS CODES AND IF NEEDED.
 WE WILL RELEASE FUTURE UPDATES SO IN ORDER TO NOT OVERWRITE YOUR CUSTOM SCRIPT IT'S BETTER LIKE THIS. */
 $(function() {
 
-	$("#example_id").ionRangeSlider();
+	var start_time = 1420070400;
+	var end_time = 1474763510;
+	var step = 604800; // 1 week in seconds
+
+	$("#example_id").ionRangeSlider({
+		'type': 'double',
+		'min': start_time,
+		'max': end_time,
+		'from': start_time,
+		'to': end_time,
+		'step': step,
+		'grid_snap': true,
+		'prettify': function(num) {
+			return moment.unix(num).format("MMM DD, YYYY");
+		},
+	});
 	
 	$('.button-collapse').sideNav({
 		menuWidth: 200,
